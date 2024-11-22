14414afe6ed5d07c32507634d79f0a1e3e0642eb
Domenik Töfflinger <domenikt96@yahoo.de>
2024-11-22 12:57:30 +0100

    feat: add canvas scroll effect

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+
+import CanvasScrollClip from "canvas-scroll-clip";
+import React, {useEffect, useRef} from "react";
+
+export default function Home() {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      new CanvasScrollClip(ref.current, {
+        framePath: "https://www.healform.de/clip3/my-film-000001.jpg",
+        frameCount: 349,
+        scrollArea: 2000,
+      });
+    }
+  }, []);
+
+  return (
+    <div className="items-center justify-items-center font-[family-name:var(--font-geist-sans)] bg-black text-white">
+      <main className="flex flex-col gap-2 items-center sm:items-start text-center">
+        <h2 className="text-center text-4xl sm:text-6xl py-16 w-full">
+          HEALFORM — die coole Therapie.
+        </h2>
+        <div className="items-center w-full" ref={ref}></div>
+        <h2 className="text-center text-4xl font-bold text-white sm:text-6xl py-16 w-full">
+          HEALFORM
+        </h2>
+      </main>
+    </div>
+  );
+}
