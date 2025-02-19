c0cf80a7f70cf481484f5e7137fc33721fdbbe20
Domenik Töfflinger <domenikt96@yahoo.de>
2025-02-19 15:14:12 +0100

    visual updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import CanvasScrollClip from "canvas-scroll-clip";
 import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import CryoniqLogo from "./../../public/logo-cryoniq.svg";
 
 export default function Home() {
   const ref = useRef(null);
@@ -16,111 +18,54 @@ export default function Home() {
     }
   }, []);
 
-  
+
 
   return (
     <div className="items-center justify-items-center font-[family-name:var(--font-geist-sans)] antialiased bg-black">
       <main className="flex flex-col gap-2 items-center sm:items-start text-center">
         <div className="flex justify-between items-center w-full p-4 bg-opacity-50 fixed top-0 left-0 right-0 z-50 align-middle">
           <div className="flex items-center">
-            <img
-              src="https://www.healform.de/img/hf_logo.png"
-              alt="Healform Logo"
+            <Image
+              src={CryoniqLogo}
+              alt="Cryoniq Logo"
               height={120}
-              width={120}
+              width={160}
             />
           </div>
-          <div className="flex gap-4 justify-between">
+          <div className="flex gap-4 justify-between items-center">
             <nav className="flex gap-4 text-white">
               <a href="#home" className="text-white">
                 Home
               </a>
               <a href="#about" className="text-white">
-                Praxis Kassel
+                Franchise
               </a>
               <a href="#services" className="text-white">
-                Praxis Baunatal
+                Services
               </a>
             </nav>
             <button className="text-white border px-4 py-2 rounded-xl text-xs">
-              Termin vereinbaren
-            </button>
-            <button className="text-white border px-4 py-2 rounded-xl text-xs">
-              Jobs
+              Kontakt
             </button>
           </div>
         </div>
 
         <div className="items-center w-full" ref={ref}></div>
 
-        <div
-          className="container mx-auto flex flex-col sm:flex-row content-center w-full pb-16 bg-violet-300 rounded-xl h-auto"
-          id="home"
-        >
+        <div className="container mx-auto flex flex-col sm:flex-row content-center w-full pb-16 bg-black border border-solid border-white/40 rounded-2xl h-auto">
           <div className="w-full p-40">
-            <h2 className="text-center text-4xl text-white sm:text-6xl pb-16">
-              HEALFORM® - Cryocenter
+            <h2 className="text-center text-4xl sm:text-6xl pb-16 text-white/40">
+              CRYO XC
             </h2>
-            <h3 className="text-center text-base text-white sm:text-xl">
-              Die erste Cryosauna in Kassel und Baunatal gilt als eine moderne
-              Ganzkörperkälteanwendung für Gesundheit*, Fitness*,
-              Leistungsfähigkeit* und Schönheit*.
-            </h3>
-            <div className="flex justify-center gap-4 mt-12">
-              <button className="text-white border border-white px-4 py-2 rounded-xl text-xs bg-transparent">
-                Email
-              </button>
-              <button className="text-white border border-white px-4 py-2 rounded-xl text-xs bg-transparent">
-                Termin vereinbaren
-              </button>
-              <button className="text-white border border-white px-4 py-2 rounded-xl text-xs bg-transparent">
-                0561 73 64 283
-              </button>
-              <button className="text-white border border-white px-4 py-2 rounded-xl text-xs bg-transparent">
-                Social Media
-              </button>
-            </div>
           </div>
         </div>
 
-     
 
-        <div className="container mx-auto flex flex-col sm:flex-row content-center w-full pb-16 bg-orange-300 rounded-lg h-auto">
+        <div className="container mx-auto flex flex-col sm:flex-row content-center w-full pb-16 bg-black border border-solid border-white/40 rounded-2xl h-auto">
           <div className="w-full p-40">
-            <h2 className="text-center text-4xl text-white sm:text-6xl pb-16">
-              Gesundheit & Schönheit
+            <h2 className="text-center text-4xl sm:text-6xl pb-16 text-white/40">
+              CRYO LC
             </h2>
-
-            <h3 className="text-center text-base text-white sm:text-xl">
-              HEALFORM® - Cryocenter bietet Ihnen ein ganzheitliches Konzept für
-              Gesundheit und Schönheit.
-            </h3>
-            <h3 className="text-center text-base text-white sm:text-xl">
-              Lebensqualität, Wohlbefinden und Vitalität möglich sind, wenn
-              Körper, Geist und Seele im Einklang sind.
-            </h3>
-          </div>
-        </div>
-       
-
-        <div className="container mx-auto flex flex-col sm:flex-row content-center w-full pb-16 bg-green-100 rounded-2xl h-auto">
-          <div className="w-full p-40">
-            <h2 className="text-center text-4xl sm:text-6xl pb-16 text-green-400">
-              Leistungssportler
-            </h2>
-
-            <div className="sm:ml-8 mt-8 sm:mt-0 text-green-400 text-center border border-green-200 rounded-xl p-4">
-              <p className="text-base sm:text-xl">
-                Unsere Cryosauna bietet zahlreiche Vorteile für
-                Leistungssportler:
-              </p>
-              <ul className="list-none mt-4 ">
-                <li>Verbesserte Regeneration</li>
-                <li>Schmerzlinderung</li>
-                <li>Reduzierung von Entzündungen</li>
-                <li>Steigerung der Leistungsfähigkeit</li>
-              </ul>
-            </div>
           </div>
         </div>
       </main>
